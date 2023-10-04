e4f78890e3691a519ca0a37671ffe5cd7c291ded
Stefe1 <smackdownmaniac@gmail.com>
2023-10-04 15:44:58 +0200

    torpedo_javitva

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
   // simulate random overheating of the launcher bay which prevents firing
-  private Random generator = new Random();//áthelyezve
+  private Random generator = new Random();
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
 
